65a61e509df02bac9cfd895cf5decf9bfc75c65e
ElieMaged <Narutossj23@yahoo.com>
2024-08-23 13:31:59 +0300

    Q

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 class MakeEspresso{
-    constructor(tea, hotWater, brand) {
+    constructor(tea, hotWater, brand, price) {
         this.tea = tea;
         this.hotWater = hotWater;
         this.brand = brand;
-   
+        let cost = price;
+        let sayPrice = function() {
+            console.log(cost)
+        }
+        this.announce = function() {
+            console.log(cost + 'HEWOOO')
+        }
     }
+
+ 
    makeTea = function() {
         console.log('We making tea UwU')
     }
@@ -13,5 +21,5 @@ class MakeEspresso{
     }
 }
 
-const blackDecker = new MakeEspresso('black', 'mild', 'Black&Decker')
+const blackDecker = new MakeEspresso('black', 'mild', 'Black&Decker', '500$')
 const kekDecker = new MakeEspresso('black', 'mild', 'Black&Decker')
