f921cc98af911d905cb95fd1277b2e5c9705d524
ElieMaged <Narutossj23@yahoo.com>
2024-08-29 18:35:21 +0300

    e

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ class Character {
            this._name = name
            this.health = health
     }
+    get name() {
+        return this._name
+    }
+    announce() {
+        console.log(`I am ${this.name}`)
+    }
 }
+
+
+class Enemy extends Character {
+    constructor(name, health, danger) {
+        super(name, health)
+        this._danger = danger
+    }
+    get encounter() {
+        return this._danger
+    }
+}
+
 
 const rochelle = new Character('Rochelle', 100) 
