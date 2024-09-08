9b0cad6af17e491fa32cbf6e892cf88f52e96bc4
ElieMaged <Narutossj23@yahoo.com>
2024-09-09 02:25:01 +0300

    q

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-const pepe = [100,2,3,4]
+class Character {
+    constructor(name, hp, mp, power) {
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+        this.power = power;
+    }
+     attack(target) {
+if(this.power > target.power) {
+    console.log(`${this.name} attacks ${target.name} for ${this.power} points of damage`);
+    target.hp -= this.power;
+    console.log(`${target.name} has ${target.hp} hp left`);
+    target.hp <= 0 ? console.log(`${target.name} has fainted!`) : console.log(`${target.name} still lives!`)
+} 
 
-function compare(a,b) {
-return a - b
+else if(target.power > this.power) {
+    console.log(`${target.name} counters ${this.name} for ${target.power} points of damage`);
+    this.hp -= target.power;
+    console.log(`${this.name} has ${this.hp} hp left`);
+}
+ 
+    }
 }
 
-console.log(pepe.sort(compare))
-  
+
+
+
+class Player extends Character {
+    constructor(name, hp, mp, secret, power) {
+    super(name, hp, mp, power);
+    this._secret = secret;
+    }
+    get secret() {
+        return this._secret
+    }
+}
+
+let aurora = new Player('Aurora', 100, 100, 'likes girls', 10)
+
+let bandit = new Character('Bandit', 50, 50, 5)
+
+
